5ac4fe68b12fa00a21ac999e58e351171ae0806f
Tiansi Hu <tiansi.hu@yahoo.com>
2018-03-07 17:13:48 -0800

    Change algo RMQ to a class.

--- alg/rmq.py
+++ alg/rmq.py
@@ -2,41 +2,40 @@
 
 import math
 
-def pre_process(array):
-    length = len(array)
-    p = int(math.log(length, 2)) + 1
+class RMQ:
+    def __init__(self, array):
+        length = len(array)
+        p = int(math.log(length, 2)) + 1
     
-    ret = []
-    for i in range(length):
-        ret.append([-1] * p)
+        self.preproc = []
+        for i in range(length):
+            self.preproc.append([-1] * p)
 
-    for i in range(length):
-        ret[i][0] = array[i]
+        for i in range(length):
+            self.preproc[i][0] = array[i]
 
-    return ret
-
-def get_value(preproc, start, power):
-    if preproc[start][power] < 0:
-        preproc[start][power] = max(get_value(preproc, start, power - 1), get_value(preproc, start + 2 **(power - 1), power - 1))
-
-    return preproc[start][power]
+    def get_value(self, start, power):
+        if self.preproc[start][power] < 0:
+            self.preproc[start][power] = max(self.get_value(start, power - 1), 
+                    self.get_value(start + 2 **(power - 1), power - 1))
+        return self.preproc[start][power]
         
-def rmq(preproc, start, length):
-    if length == 1:
-        return preproc[start][0]
+    def rmq(self, start, length):
+        if length == 1:
+            return self.preproc[start][0]
 
-    k = int(math.log(length, 2)) 
-    return max(get_value(preproc, start, k), get_value(preproc, start + length - 2 ** k, k))
+        k = int(math.log(length, 2)) 
+        return max(self.get_value(start, k), self.get_value(start + length - 2 ** k, k))
 
 if __name__ == "__main__":
     array = [4, 1, 3, 7, 81, 1, 1, 5, 7];
 
-    preproc = pre_process(array)
+    rmq = RMQ(array)
 
     length = len(array)
 
     for i in range(length):
         for j in range(1, length - i + 1):
-            r = rmq(preproc, i, j)
+            r = rmq.rmq(i, j)
             print "%d:%d %d" % (i, j, r)
 
