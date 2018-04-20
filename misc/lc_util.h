fcd76ef3374035fe8b1185f2f73c06a251a12f1a
Tiansi Hu <tiansi.hu@yahoo.com>
2018-04-20 15:06:15 -0700

    Add more stuff.

--- misc/lc_util.h
+++ misc/lc_util.h
@@ -1,5 +1,9 @@
 #ifndef __LC_UTIL_H__
 #define __LC_UTIL_H__
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 struct ListNode {
     int val;
@@ -7,6 +11,20 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+ListNode *make_list(const vector<int> &a) {
+    ListNode *head = NULL;
+    ListNode *p = NULL;
+    for (const int &i: a) {
+        ListNode *q = new ListNode(i);
+        if (head == NULL) {
+            head = p = q;
+        } else {
+            p->next = q;
+            p = q;
+        }
+    }
+    return head;
+}
 
 void print_list(ListNode *head){
     while (head != NULL) {
